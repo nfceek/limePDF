933a78553e107d2aadae850de1944af42556f660
nfceek <bsmith@nfceek.com>
2025-09-01 10:01:25 -0400

    boilerplate Barcodes 2D QRCODES

--- examples/barcodes/example_2d_qrcode_svgi.php
+++ examples/barcodes/example_2d_qrcode_svgi.php
@@ -1,52 +1,25 @@
-<?php
+<?php 
 //============================================================+
-// File name   : example_2d_svgi.php
-// Version     : 1.0.000
-// Begin       : 2011-07-21
-// Last Update : 2013-03-19
-// Author      : Nicola Asuni - Tecnick.com LTD - www.tecnick.com - info@tecnick.com
-// License     : GNU-LGPL v3 (http://www.gnu.org/copyleft/lesser.html)
-// -------------------------------------------------------------------
-// Copyright (C) 2009-2013 Nicola Asuni - Tecnick.com LTD
+// File name: example_2d_pdf417_png.php
 //
-// This file is part of TCPDF software library.
+// Author: Brad Smith
+// (c) Copyright 2025, Brad Smith - LimePDF.com
 //
-// TCPDF is free software: you can redistribute it and/or modify it
-// under the terms of the GNU Lesser General Public License as
-// published by the Free Software Foundation, either version 3 of the
-// License, or (at your option) any later version.
+//  * Original TCPDF Copyright (c) 2002-2023:
+//  * Nicola Asuni - Tecnick.com LTD - info@tecnick.com
 //
-// TCPDF is distributed in the hope that it will be useful, but
-// WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-// See the GNU Lesser General Public License for more details.
 //
-// You should have received a copy of the GNU Lesser General Public License
-// along with TCPDF.  If not, see <http://www.gnu.org/licenses/>.
+// Description : example_2d_qrcode_svgi..php
+//               
 //
-// See LICENSE.TXT file for more information.
-// -------------------------------------------------------------------
-//
-// Description : Example for tcpdf_barcodes_2d.php class
-//
+// Last Update : 9-1-2025
 //============================================================+
 
-/**
- * @file
- * Example for tcpdf_barcodes_2d.php class
- * @package com.tecnick.tcpdf
- * @author Nicola Asuni
- * @version 1.0.009
- * @group barcode
- * @group qrcode
- * @group svg
- */
-
-// include 2D barcode class (search for installation path)
-require_once(dirname(__FILE__).'/tcpdf_barcodes_2d_include.php');
+use LimePDF\Barcodes\Barcodes2D;
+require_once __DIR__ . '/../../src/Barcodes/Barcodes2D.php';
 
 // set the barcode content and type
-$barcodeobj = new TCPDF2DBarcode('http://www.tcpdf.org', 'QRCODE,H');
+$barcodeobj = new Barcodes2D('https://limepdf.com', 'QRCODE,H');
 
 // output the barcode as SVG inline code
 echo $barcodeobj->getBarcodeSVGcode(6, 6, 'black');
